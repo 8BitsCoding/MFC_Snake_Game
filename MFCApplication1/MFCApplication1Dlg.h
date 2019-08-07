27a735a701643dd4ec4de2395c749c26b1db1530
taehyeong77@naver.com <taehyeong77@naver.com>
2019-08-07 14:28:48 +0900

    commit

---
+++ MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,50 @@
+﻿
+// MFCApplication1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCApplication1Dlg 대화 상자
+class CMFCApplication1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplication1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+public:
+	CImage m_draw_image;
+	CDC m_draw_dc;
+
+	COLORREF m_table[60][80] = { 0, };
+	int m_count_map[60][80] = { 0, };
+	POINT m_pos = { 40, 30 };		// 녹색 뱀의 위치
+	int m_direction = 0;			// 0 left / 1 up / 2 right / 3 bottom
+	int m_eat_count = 0;
+
+	afx_msg void OnDestroy();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	void DrawMap();
+	void GameOver();
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	afx_msg void OnEnChangeEdit1();
+};
